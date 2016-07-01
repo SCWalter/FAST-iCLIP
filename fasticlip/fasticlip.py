6edeaed002e1a35326e9961c37a2d08e9fd49c1e
SCWalter <shinji.ikari@live.com>
2016-07-01 10:39:30 -0700

    Revert back for "indexing-view-versus-copy"

--- fasticlip/fasticlip.py
+++ fasticlip/fasticlip.py
@@ -412,8 +412,8 @@ def main():
 		end=repeatAnnotDF.loc[ix,'IndexEnd']
 		repName=repeatAnnotDF.loc[ix,'Name']
 		gene_hits=rep[(rep['RT_stop']<int(repeatAnnotDF.loc[ix,'IndexEnd']))&(rep['RT_stop']>int(repeatAnnotDF.loc[ix,'IndexStart']))]
-		gene_hits.loc[:,'Repeat_End']=repeatAnnotDF.loc[ix,'IndexEnd']
-		gene_hits.loc[:,'Repeat_Start']=repeatAnnotDF.loc[ix,'IndexStart']
+		gene_hits['Repeat_End']=repeatAnnotDF.loc[ix,'IndexEnd']
+		gene_hits['Repeat_Start']=repeatAnnotDF.loc[ix,'IndexStart']
 		outfilepathToSave=cfg.outfilepath + '/PlotData_RepeatRNAreads_%s'%repName
 		gene_hits.to_csv(outfilepathToSave)
 	
